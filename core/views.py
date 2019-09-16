0e87f29cb47d26cf1f37c53799c523b27b5383e2
Cleiton de Lima <cleiton.limapin@gmail.com>
2019-09-16 13:01:03 -0300

    Nova url /bills/balance/, mostra o saldo das contas

--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
+from django.db.models import Count, Sum, Q
 from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from core.models import Record, Category, Bill
 from core.serializers import (
@@ -32,3 +35,28 @@ class BillViewSet(mixins.CreateModelMixin,
                   viewsets.GenericViewSet):
     serializer_class = BillSerializer
     queryset = Bill.objects.all()
+
+    @action(detail=False)
+    def balance(self, request, pk=None):
+        queryset = self.get_queryset()
+        queryset = queryset.annotate(
+            expense_count=Count('records', filter=Q(records__type=Record.OUT)),
+            income_count=Count('records', filter=Q(records__type=Record.IN)),
+            balance=(
+                    Sum('records__value', filter=Q(records__type=Record.IN)) -
+                    Sum('records__value', filter=Q(records__type=Record.OUT))
+            )
+        )
+
+        data = [
+            dict(
+                id=b.id,
+                name=b.name,
+                expense_count=b.expense_count,
+                income_count=b.income_count,
+                balance=b.balance
+            )
+            for b in queryset
+        ]
+
+        return Response(data)
